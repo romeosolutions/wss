32303857baa78be45aac05b755304fe75bdfcb3d
Bhavit kanthalia <bhavitkanthalia16@gmail.com>
2020-08-16 21:38:17 +0530

    add packages and material design dependency

--- wss/app/src/main/java/com/example/wss/auth/RegistrationActivity.kt
+++ wss/app/src/main/java/com/example/wss/auth/RegistrationActivity.kt
@@ -1,12 +1,13 @@
-package com.example.wss
+package com.example.wss.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wss.R
 
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registration)
     }
 }
